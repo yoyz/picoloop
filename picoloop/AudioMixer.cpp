a635d273019d5c2621390a6554918cf0c5c6bee2
yoyz <yoyz2k@yahoo.fr>
2013-08-28 23:36:53 +0200

    Really dirty audiomixer with two input
    +  Sint32 sint32tick=(T0.tick()/2)+(T1.tick()/2);

--- picoloop/AudioMixer.cpp
+++ picoloop/AudioMixer.cpp
@@ -4,17 +4,25 @@ AudioMixer::AudioMixer()
 {
 }
 
-Track & AudioMixer::getTrack()
+Track & AudioMixer::getTrack(int numTrack)
 {
-  return T;
+  if (numTrack==0)
+    return T0;
+  else
+    return T1;
 }
 
-int AudioMixer::tick()
+Sint16 AudioMixer::tick()
 {
-  int inttick=T.tick();
+  //Sint16 inttick=T.tick();
+  Sint32 sint32tick=(T0.tick()/2)+(T1.tick()/2);
+  Sint16 sint16tick;
+  
+  sint16tick=sint32tick;
   //  printf("%d\n",inttick);
   //  return S.tick();
-  return inttick;
+  //return inttick;
+  return sint16tick;
 }
 
 
