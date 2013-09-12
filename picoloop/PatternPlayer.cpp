f7ff6cd16d8862fab0f99b27de96d28eab29279e
yoyz <yoyz2k@yahoo.fr>
2013-09-12 22:38:55 +0200

    Now with SDL

--- picoloop/PatternPlayer.cpp
+++ picoloop/PatternPlayer.cpp
@@ -10,6 +10,7 @@ using namespace std;
 #include "AudioEngine.h"
 #include "Wave.h"
 #include "MonoMixer.h"
+#include "SDL_GUI.h"
 
 AudioEngine ae;
 
@@ -19,12 +20,59 @@ Pattern P1;
 PatternElement PE0;
 PatternElement PE1;
 
+SDL_GUI sg;
+
 //MonoMixer MM0;
 //MonoMixer MM1;
 
 Wave cowbell;
 //Synth beeper;
 Instrument inst;
+
+int quit;
+bool left_key=false;
+bool right_key=false;
+bool up=false;
+bool down=false;
+bool pageup=false;
+bool pagedown=false;
+bool ctrl=false;
+
+bool q_key=false; //C
+bool z_key=false; //C+
+bool s_key=false; //D
+bool e_key=false; //D+
+bool d_key=false; //E
+bool f_key=false; //F
+bool t_key=false; //F+
+bool g_key=false; //G
+bool y_key=false; //G+
+bool h_key=false; //A
+bool u_key=false; //A+
+bool j_key=false; //B
+
+bool k_key=false; //C
+bool o_key=false; //C+
+bool l_key=false; //D
+bool p_key=false; //D+
+bool m_key=false; //E
+
+
+int cursor=0;
+
+int attack=0;
+int release=0;
+
+void display_board()
+{
+  int i;
+  for (i=0;i<16;i++)
+    {
+      sg.drawBoxNumber(i,0xAECD15);
+    }
+  sg.refresh();
+}
+
 
 void openaudio()
 {
@@ -36,6 +84,207 @@ void openaudio()
   ae.openAudio();
   printf("After\n");
 }
+
+
+void handle_key()
+{
+  SDL_Event event;
+  if (SDL_PollEvent(&event))
+    {
+      // printf("loop %d\n",loop);
+      //loop++;
+      
+      //printf("[%d %d %d %d]\n",SDL_KEYUP,SDL_KEYDOWN,event.type,event.key.keysym.sym);
+      switch (event.type)
+	{
+	case SDL_QUIT:
+	  printf("Exiting...\n");
+	  quit = 1;	  
+	  exit(0);
+	  break; 	  
+	  
+	case SDL_KEYUP:
+	  switch (event.key.keysym.sym)
+	    {
+	    case SDLK_LCTRL:       ctrl=false;      break;
+	    case SDLK_RCTRL:       ctrl=false;      break;
+	    case SDLK_LEFT:        left_key=false;  break;	     
+	    case SDLK_RIGHT:       right_key=false; break;	      
+	    case SDLK_UP:          up=false;        break;	      
+	    case SDLK_DOWN:        down=false;      break;
+	    case SDLK_PAGEUP:      pageup=false;    break;	      
+	    case SDLK_PAGEDOWN:    pagedown=false;  break;
+	      
+	    case SDLK_q:           q_key=false;     break;
+	    case SDLK_z:           z_key=false;     break;
+	    case SDLK_s:           s_key=false;     break;
+	    case SDLK_e:           e_key=false;     break;
+	    case SDLK_d:           d_key=false;     break;
+	    case SDLK_f:           f_key=false;     break;
+	    case SDLK_t:           t_key=false;     break;
+	    case SDLK_g:           g_key=false;     break;
+	    case SDLK_y:           y_key=false;     break;
+	    case SDLK_h:           h_key=false;     break;
+	    case SDLK_u:           u_key=false;     break;
+	    case SDLK_j:           j_key=false;     break;
+	    case SDLK_k:           k_key=false;     break;
+	    case SDLK_o:           o_key=false;     break;
+	    case SDLK_l:           l_key=false;     break;
+	    case SDLK_p:           m_key=false;     break;
+	    case SDLK_m:           m_key=false;     break;
+	      
+	      
+	      
+	    default:
+	      break;
+	    }
+	  break;
+	  
+	case SDL_KEYDOWN:
+	  switch (event.key.keysym.sym)
+	    {
+	    case SDLK_ESCAPE:
+	      printf("Exiting...\n");
+	      quit = 1;
+	      exit(0);
+	      break;
+	      
+	    case SDLK_LCTRL:       ctrl=true;      break;
+	    case SDLK_RCTRL:       ctrl=true;      break;
+	    case SDLK_LEFT:        left_key=true;  break;	      
+	    case SDLK_RIGHT:       right_key=true; break;	      
+	    case SDLK_UP:          up=true;        break;	      
+	    case SDLK_DOWN:        down=true;      break;
+	    case SDLK_PAGEUP:      pageup=true;    break;	      
+	    case SDLK_PAGEDOWN:    pagedown=true;  break;
+	      
+	    case SDLK_q:           q_key=true;     break;
+	    case SDLK_z:           z_key=true;     break;
+	    case SDLK_s:           s_key=true;     break;
+	    case SDLK_e:           e_key=true;     break;
+	    case SDLK_d:           d_key=true;     break;
+	    case SDLK_f:           f_key=true;     break;
+	    case SDLK_t:           t_key=true;     break;
+	    case SDLK_g:           g_key=true;     break;
+	    case SDLK_y:           y_key=true;     break;
+	    case SDLK_h:           h_key=true;     break;
+	    case SDLK_u:           u_key=true;     break;
+	    case SDLK_j:           j_key=true;     break;
+	    case SDLK_k:           k_key=true;     break;
+	    case SDLK_o:           o_key=true;     break;
+	    case SDLK_l:           l_key=true;     break;
+	    case SDLK_p:           m_key=true;     break;
+	    case SDLK_m:           m_key=true;     break;
+	      
+	      
+	    default:
+	      break;
+	      
+	    }
+	  
+	  break;
+	  
+	}
+      printf("new event:%d %s\n",event.type,SDL_GetKeyName(event.key.keysym.sym));
+    }
+  
+  //printf("No new event\n");
+  
+  
+  if(up)
+    {
+      cursor=cursor-4;
+      if (cursor < 0) cursor=cursor +16;
+      printf("key down\n");
+    }
+
+  if(down)
+    {
+      cursor=( cursor+4 ) %16;
+      //if (cursor > 15) cursor=cursor-12;
+      printf("[cursor:%d]\n",cursor);
+      printf("key down\n");
+    }
+
+  if(left_key)
+    {
+      bool moved=false;
+      /*
+      if (cursor=0)     { cursor=3;  moved=true;   }
+      if (cursor=4)     { cursor=7;  moved=true;   }
+      if (cursor=8)     { cursor=11;  moved=true;   }
+      if (cursor=12)    { cursor=15; moved=true;   }
+      if (moved==false) { cursor++;  }
+      */
+      cursor--;
+      if (cursor<0) cursor=15;
+      printf("[cursor:%d]\n",cursor);
+      printf("key left\n");            
+    }
+
+  if (right_key)
+    {
+      bool moved=false;
+      /*
+      if (cursor=3)     { cursor=0;  moved=true;   }
+      if (cursor=7)     { cursor=4;  moved=true;   }
+      if (cursor=11)    { cursor=8;  moved=true;   }
+      if (cursor=15)    { cursor=12; moved=true;   }
+      if (moved==false) { cursor++;  }
+      */
+      cursor++;
+      printf("key right\n");      
+    }
+
+  if(pageup)
+    {
+      printf("key pgup \n");      
+    }
+
+  if(pagedown)
+    {
+      printf("key pgdown \n");
+    }
+
+  if (s_key) { attack=-1;  }
+  if (z_key) { attack=1;   }
+  if (e_key) { release=-1; }
+  if (d_key) { release=1;  }
+
+
+
+  /*
+  if (k_key)
+    {
+            printf("key z\n");
+	    //Machine    & m0=ae.getAudioMixer().getTrack(0).getMachine();
+	    m0.setSynthFreq(880);
+    }
+  */
+
+  //No user activity => sleeping a while to keep cpu cool
+  //it keep cpu under 100% usage
+  if ((up       || 
+       down     || 
+       left     || 
+       right    || 
+       pagedown || 
+       pageup   ||
+       q_key    ||
+       z_key    ||
+       k_key    
+       )==false)
+  {
+    //      printf("sleeping 100ms");
+    //      SDL_Delay(100);
+  }
+  else
+    {
+      //printf("sleeping 10ms");
+      //SDL_Delay(120);
+    }
+}
+
 
 
 int seq()
@@ -98,7 +347,26 @@ int seq()
       nbcb=ae.getNbCallback();
       if (nbcb-last_nbcb>nb_cb_ch_step)
 	{
-	  
+	  //printf("[cursor:%d]\n",cursor);
+	  handle_key();
+
+	  if (attack!=0)
+	    {
+	      //m0.getADSR().setAttack(m0.getADSR().getAttack()+attack);
+	      P0.getPatternElement(cursor).setAttack(P0.getPatternElement(cursor).getAttack()+attack);
+	      attack=0;
+	      printf("[attack:%d]\n",m0.getADSR().getAttack());
+	    }
+
+	  if (release!=0)
+	    {
+	      //	      m0.getADSR().setRelease(m0.getADSR().getRelease()+release);
+	      P0.getPatternElement(cursor).setRelease(P0.getPatternElement(cursor).getRelease()+release);
+	      release=0;
+	      printf("[release:%d %d]\n",m0.getADSR().getRelease(),P0.getPatternElement(cursor).getRelease()+release);
+
+	    }
+
 	  //if (step%3==0) { m.setSineOsc(); }
 	  //if (step%3==1) { m.setSawOsc(); }
 	  //if (step%3==2) { m.setFuzzyPulseOsc(); }
@@ -109,8 +377,22 @@ int seq()
 	  //m0.setSineOsc();
 	  //m1.setSineOsc();
 	  
+
+
+
 	  if (step==16) { step=0; }
 	  printf("STEP:%d\n",step);	  
+
+	  display_board();
+	  //GRAPHICS
+	  sg.drawBoxNumber(step,0x045c15);
+	  //if (step > 0)  sg.drawBoxNumber(step-1,0xAECD15);
+	  //	  if (step == 0) sg.drawBoxNumber(15,0xAECD15);
+
+	  sg.drawBoxNumber(cursor,0x1545CD);
+	  sg.refresh();
+	  //GRAPHICS
+	  
 
 	  //printf("loop\n");    
 	  last_nbcb=nbcb;
@@ -118,18 +400,25 @@ int seq()
 	    {
 	      float f=P0.getPatternElement(step).getNoteFreq();
 	      int   i=f;
-		
-	      printf("%f %d \n",P0.getPatternElement(step).getNoteFreq(),i);
+	      
+	      //printf("%f %d \n",P0.getPatternElement(step).getNoteFreq(),i);
 	      m0.setSynthFreq(i);
 	      m0.getADSR().reset();
+	      //              m0.getADSR().setAttack(P0.getPatternElement(step).getAttack());
+	      //	      m0.getADSR().setDecay(P0.getPatternElement(step).getDecay());
+	      //	      m0.getADSR().setSustain(P0.getPatternElement(step).getSustain());
+	      m0.getADSR().setRelease(P0.getPatternElement(step).getRelease());
+
+
+	      //m0.getADSR().get();
 	      m0.getOscillator()->reset();
 	      //m.setSynthFreq(1200);
 	    }
 	  else
 	    {
 	      //m.setSynthFreq(800);
-	      printf("m.setSynthFreq(0);\n");
-	      m0.setSynthFreq(0);
+	      //printf("m.setSynthFreq(0);\n");
+	      //m0.setSynthFreq(0);
 	    }
 
 	  if (P1.getPatternElement(step).getTrig()==true)
@@ -137,7 +426,7 @@ int seq()
 	      float f=P1.getPatternElement(step).getNoteFreq();
 	      int   i=f;
 		
-	      printf("%f %d \n",P1.getPatternElement(step).getNoteFreq(),i);
+	      //printf("%f %d \n",P1.getPatternElement(step).getNoteFreq(),i);
 	      m1.setSynthFreq(i);
 	      m1.getADSR().reset();
 	      m1.getOscillator()->reset();
@@ -150,16 +439,20 @@ int seq()
 	      m1.setSynthFreq(0);
 	    }
 
-
 	  step++;  
 	}
+
+      //Keyboard
+      
+      //Keyboard
+
       //am.setSynthFreq(880);
       //am.setSynthFreq(880);
       //ae.getAudioMixer().setSynthFreq(440);
       //t.setSynthFreq(880);
       //m.setSynthFreq(880);
       //printf("loop\n");
-      usleep(1000);
+      usleep(10);
       //t.setSynthFreq(1660);
       //am.setSynthFreq(1660);
       //ae.getAudioMixer().setSynthFreq(880);
@@ -184,7 +477,7 @@ void setPattern()
   //PR.setFileName("data.pic");
   PR.setFileName(fileName);
   PR.readPatternData(1,1,P0);
-  PR.readPatternData(1,2,P1);
+  //  PR.readPatternData(1,2,P1);
 
   /*
   P0.setPatternSize(16);
@@ -240,12 +533,19 @@ void setPattern()
   */
 }
 
+
 int main()
 {
   //string wave="808-cowbell.wav";
   char * str="808-cowbell.wav";
   cowbell.loadWave(str);
   setPattern();
+  sg.initVideo();
+  //SDL_EnableKeyRepeat(25, );
+  sg.openFont();
+  display_board();
+
+
   openaudio();
   seq();
   //sleep(10);
