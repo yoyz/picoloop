074c86989733a101cbd75133875a1d02afae8fba
yoyz <yoyz2k@yahoo.fr>
2013-09-12 22:40:43 +0200

    update of SDL_GUI

--- picoloop/Picoloop.cpp
+++ picoloop/Picoloop.cpp
@@ -16,20 +16,22 @@ int main()
       sg.drawBoxNumber(i,0xAECD15);
     }
   sg.refresh();
-  for (j=0;j<1;j++)
+  for (j=0;j<1000;j++)
     for (i=0;i<16;i++)
       {
 	sg.drawBoxNumber(i,0x045c15);
 	if (i > 0)  sg.drawBoxNumber(i-1,0xAECD15);
 	if (i == 0) sg.drawBoxNumber(15,0xAECD15);
-	sg.refresh();
-	usleep(100000);
-	//printf("%d\n",i);
 	sg.guiText(20,24,"C4");
+	sg.refresh();
+	//usleep(100);
+	SDL_Delay(100);
+	//usleep(400000);
+	//sleep(1);o
+	printf("%d\n",i);
+
       }
-
   
-
   sleep(1);
   sg.closeVideo();
   return(0);
