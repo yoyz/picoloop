62ee2a14d40d35061dd178d1e0fa38a6f07261e4
yoyz <yoyz2k@yahoo.fr>
2013-09-15 14:44:43 +0200

    test small box

--- picoloop/Picoloop.cpp
+++ picoloop/Picoloop.cpp
@@ -5,31 +5,58 @@ SDL_GUI sg;
 
 int main()
 {
-  int i;
-  int j;
+  int i=0;
+  int j=0;
+  int k=0;
+  int l=0;
   sg.initVideo();
   sg.openFont();
   /*
   */
-  for (i=0;i<16;i++)
+  for (l=0;l<16;l++)
     {
-      sg.drawBoxNumber(i,0xAECD15);
+      sg.drawBoxNumber(l,0xAECD15);
     }
   sg.refresh();
   for (j=0;j<1000;j++)
     for (i=0;i<16;i++)
       {
-	sg.drawBoxNumber(i,0x045c15);
+	for (l=0;l<16;l++) 
+	  sg.drawBoxNumber(l,0xAECD15);
+
 	if (i > 0)  sg.drawBoxNumber(i-1,0xAECD15);
 	if (i == 0) sg.drawBoxNumber(15,0xAECD15);
-	sg.guiText(20,24,"C4");
+	sg.drawBoxNumber(i,0x045c15);
+
+
+	sg.smallBoxNumber(0,k,k,0x443322);
+	sg.smallBoxNumber(1,k,k,0x443322);
+	sg.smallBoxNumber(2,k,k,0x443322);
+	sg.smallBoxNumber(3,k,k,0x443322);
+
+	sg.smallBoxNumber(4,k,k,0x443322);
+	sg.smallBoxNumber(5,k,k,0x443322);
+	sg.smallBoxNumber(6,k,k,0x443322);
+	sg.smallBoxNumber(7,k,k,0x443322);
+
+	sg.smallBoxNumber(8,k,k,0x443322);
+	sg.smallBoxNumber(9,k,k,0x443322);
+	sg.smallBoxNumber(10,k,k,0x443322);
+	sg.smallBoxNumber(11,k,k,0x443322);
+
+	sg.smallBoxNumber(12,k,k,0x443322);
+	sg.smallBoxNumber(13,k,k,0x443322);
+	sg.smallBoxNumber(14,k,k,0x443322);
+	sg.smallBoxNumber(15,k,k,0x443322);
+
+	//sg.guiText(20,24,"C4");
 	sg.refresh();
 	//usleep(100);
 	SDL_Delay(100);
 	//usleep(400000);
 	//sleep(1);o
-	printf("%d\n",i);
-
+	printf("%d %d %d\n",i,j,k);
+	k++;
       }
   
   sleep(1);
