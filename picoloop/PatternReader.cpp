612ff3769e45392a45dd9da11742df6ae462f51c
yoyz <yoyz2k@yahoo.fr>
2014-05-01 13:59:14 +0200

    cutoff+resonance

--- picoloop/PatternReader.cpp
+++ picoloop/PatternReader.cpp
@@ -341,6 +341,48 @@ bool PatternReader::readPatternData(int PatternNumber,int TrackNumber, Pattern &
   else
     retcode=false;
 
+  fgets(line,512,fd);
+  //match('Pattern 1 Track 1 Param Release 0  0 1  0 0  0 1  0 0  1  0  0  1  0  0')
+  sscanf(line,
+	 "Pattern %d Track %d Param Cutoff %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+	 &PatNum,&TrackNum,
+	 &t[0], &t[1], &t[2], &t[3],
+	 &t[4], &t[5], &t[6], &t[7],
+	 &t[8], &t[9], &t[10],&t[11],
+	 &t[12],&t[13],&t[14],&t[15]);
+
+  if (PatNum    ==PatternNumber &&
+      TrackNum  ==TrackNumber)
+    for (i=0;i<PatSize;i++)
+      {
+	Pe=P.getPatternElement(i);    
+	Pe.setCutoff(t[i]);  
+	P.setPatternElement(i,Pe);      
+      }
+  else
+    retcode=false;
+
+  fgets(line,512,fd);
+  //match('Pattern 1 Track 1 Param Release 0  0 1  0 0  0 1  0 0  1  0  0  1  0  0')
+  sscanf(line,
+	 "Pattern %d Track %d Param Resonance %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+	 &PatNum,&TrackNum,
+	 &t[0], &t[1], &t[2], &t[3],
+	 &t[4], &t[5], &t[6], &t[7],
+	 &t[8], &t[9], &t[10],&t[11],
+	 &t[12],&t[13],&t[14],&t[15]);
+
+  if (PatNum    ==PatternNumber &&
+      TrackNum  ==TrackNumber)
+    for (i=0;i<PatSize;i++)
+      {
+	Pe=P.getPatternElement(i);    
+	Pe.setResonance(t[i]);  
+	P.setPatternElement(i,Pe);      
+      }
+  else
+    retcode=false;
+
 
 
 
@@ -489,7 +531,26 @@ bool PatternReader::writePattern(int PatternNumber, int TrackNumber, Pattern & P
     {
       if (i==0)
 	sprintf(line,"Pattern %d Track %d Param OscTwoType ",PatternNumber,TrackNumber);
-      sprintf(line+strlen(line),"%d ",P.getPatternElement(i).getOscillatorTwoType());
+      sprintf(line+strlen(line),"%d ",P.getPatternElement(i).getOscillatorTwoType());    
+    }
+  sprintf(line+strlen(line),"\n");
+  data.insert(data.end(),line);
+
+
+  for (int i=0; i< P.getSize();i++)
+    {
+      if (i==0)
+	sprintf(line,"Pattern %d Track %d Param Cutoff ",PatternNumber,TrackNumber);
+      sprintf(line+strlen(line),"%d ",P.getPatternElement(i).getCutoff());
+    }
+  sprintf(line+strlen(line),"\n");
+  data.insert(data.end(),line);
+
+  for (int i=0; i< P.getSize();i++)
+    {
+      if (i==0)
+	sprintf(line,"Pattern %d Track %d Param Resonance ",PatternNumber,TrackNumber);
+      sprintf(line+strlen(line),"%d ",P.getPatternElement(i).getResonance());
     }
   sprintf(line+strlen(line),"\n");
   data.insert(data.end(),line);
