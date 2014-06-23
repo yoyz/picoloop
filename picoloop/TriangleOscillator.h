0a5bb96a3367e4807911cec74d3626cc771b501b
yoyz <yoyz2k@yahoo.fr>
2014-06-23 19:15:15 +0200

    implementation of triangle

---
+++ picoloop/TriangleOscillator.h
@@ -0,0 +1,30 @@
+using namespace std;
+
+#include <string>
+#include <stdio.h>
+#include <math.h>
+#include <limits>
+#include "Wave.h"
+//#include "AudioEngine.h"
+#include "Oscillator.h"
+
+#ifndef __TRIANGLEOSCILLATOR__
+#define __TRIANGLEOSCILLATOR__
+
+class TriangleOscillator : public Oscillator
+{
+ public:
+  TriangleOscillator();
+  ~TriangleOscillator();
+  //  void   setFreq(int freq);
+  //  void   setAmplitude(int amplitude);
+  Sint16 * table;
+  void     init();
+  Sint16   tick();
+  //  int      phase;  
+  int      table_size;
+  int      index;
+  
+};
+
+#endif
