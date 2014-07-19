2009be4a14a36fb8698bdb02bff5cea3c3129d04
yoyz <yoyz2k@yahoo.fr>
2014-07-19 13:25:37 +0200

    fix build_debian.sh

--- picoloop/build_debian.sh
+++ picoloop/build_debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-make -j 64 -f Makefile.RtAudio clean
-make -j 64 -f Makefile.PatternPlayer  clean
+make -j 64 -f Makefile.RtAudio_debian clean
+make -j 64 -f Makefile.PatternPlayer_debian_RtAudio clean
 
-make -j 64 -f Makefile.RtAudio
-make -j 64 -f Makefile.PatternPlayer
+make -j 64 -f Makefile.RtAudio_debian
+make -j 64 -f Makefile.PatternPlayer_debian_RtAudio
 
