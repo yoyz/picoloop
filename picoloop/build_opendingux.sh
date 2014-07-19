bc0872b4815abff7c7a93916a82bffcbd7009074
yoyz <yoyz2k@yahoo.fr>
2014-07-19 16:52:41 +0200

    makefile fix

--- picoloop/build_opendingux.sh
+++ picoloop/build_opendingux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-make -j 64 -f Makefile.RtAudio clean
-make -j 64 -f Makefile.PatternPlayer  clean
+make -j 64 -f Makefile.RtAudio_opendingux               clean
+make -j 64 -f Makefile.PatternPlayer_opendingux_RtAudio clean
 
-make -j 64 -f Makefile.RtAudio_opendingux
-make -j 64 -f Makefile.PatternPlayer_opendingux 
+make -j 64 -f Makefile.RtAudio_opendingux     
+make -j 64 -f Makefile.PatternPlayer_opendingux_RtAudio
 
