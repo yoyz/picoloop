4ceaef351348c508f0647092667d59c6c83508e0
yoyz <yoyz2k@yahoo.fr>
2014-08-31 18:10:14 +0200

    now it save the machine

--- picoloop/PatternReader.cpp
+++ picoloop/PatternReader.cpp
@@ -657,6 +657,28 @@ bool PatternReader::readPatternData(int PatternNumber,int TrackNumber, Pattern &
     retcode=false;
 
 
+  fgets(line,512,fd);
+  //match('Pattern 1 Track 1 Param Release 0  0 1  0 0  0 1  0 0  1  0  0  1  0  0')
+  sscanf(line,
+	 "Pattern %d Track %d Param MachineType %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+	 &PatNum,&TrackNum,
+	 &t[0], &t[1], &t[2], &t[3],
+	 &t[4], &t[5], &t[6], &t[7],
+	 &t[8], &t[9], &t[10],&t[11],
+	 &t[12],&t[13],&t[14],&t[15]);
+
+  if (PatNum    ==PatternNumber &&
+      TrackNum  ==TrackNumber)
+    for (i=0;i<PatSize;i++)
+      {
+	Pe=P.getPatternElement(i);      
+	Pe.setMachineType(t[i]);
+	P.setPatternElement(i,Pe);      
+      }
+  else
+    retcode=false;
+
+
 
   if (retcode==true)
     loadedData[PatternNumber][TrackNumber]=DATA_EXIST_ON_STORAGE;
@@ -944,6 +966,15 @@ bool PatternReader::writePattern(int PatternNumber, int TrackNumber, Pattern & P
       if (i==0)
 	sprintf(line,"Pattern %d Track %d Param NoteADSR ",PatternNumber,TrackNumber);
       sprintf(line+strlen(line),"%d ",P.getPatternElement(i).getNoteADSR());
+    }
+  sprintf(line+strlen(line),"\n");
+  data.insert(data.end(),line);
+
+  for (int i=0; i< P.getSize();i++)
+    {
+      if (i==0)
+	sprintf(line,"Pattern %d Track %d Param MachineType ",PatternNumber,TrackNumber);
+      sprintf(line+strlen(line),"%d ",P.getPatternElement(i).getMachineType());
     }
   sprintf(line+strlen(line),"\n");
   data.insert(data.end(),line);
