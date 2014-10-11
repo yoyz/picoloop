a955e1efdfc1c6ae73e800186e7ab7bc43c99c68
yoyz <yoyz2k@yahoo.fr>
2014-10-11 15:31:32 +0200

    cosmetic change loading screen

--- picoloop/SDL_GUI.cpp
+++ picoloop/SDL_GUI.cpp
@@ -108,7 +108,7 @@ int SDL_GUI::closeVideo()
 
 void SDL_GUI::loadingScreen()
 {
-  char txt[128]="Loading";
+  char txt[128]="Loading picoloop";
   this->guiTTFText((SCREEN_WIDTH/2)*SCREEN_MULT,(SCREEN_HEIGHT/2)*SCREEN_MULT,txt);
   this->refresh();
 }
