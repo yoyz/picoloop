ef3efaabbabbb8c2007d345969f648d03c54e0e1
yoyz <yoyz2k@yahoo.fr>
2014-10-12 17:37:00 +0200

    add the build_windows script

---
+++ picoloop/build_windows.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+make       -f Makefile.RtAudio_mingw32                              clean
+make       -f Makefile.Makefile.PatternPlayer_windows_mingw_RtAudio clean
+
+make       -f Makefile.RtAudio_mingw32                              
+make       -f Makefile.Makefile.PatternPlayer_windows_mingw_RtAudio -j 64
+
+
