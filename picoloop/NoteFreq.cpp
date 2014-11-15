e806cd5115c3671d1e3cc1c657496d67ba72d04c
yoyz <yoyz2k@yahoo.fr>
2014-11-15 15:47:56 +0100

    move fixed point frequency from 16.16 32 bit to 24.8 32 bit

--- picoloop/NoteFreq.cpp
+++ picoloop/NoteFreq.cpp
@@ -30,6 +30,9 @@ void NoteFreq::init()
   float wtjump_detune_2;
   float value;
 
+  const int   shift=8;
+  const int   wtshift=1<<shift;
+
   if (wtjTable!=0)
     return;
 
@@ -47,17 +50,18 @@ void NoteFreq::init()
 	wtjump_detune=(((this->getFNoteFreq(i+1)*(j)))-((this->getFNoteFreq(i-1)*(128-j))))/(128*12);
 	wtjTable[i][j]=wtjump_freq+wtjump_detune;
 	*/
-	wtjump_freq=(freq*WAVETABLE_SIZE*65535)/44100;	
+	wtjump_freq=(freq*WAVETABLE_SIZE*(wtshift))/44100;	
 	//wtjump_detune=(((this->getFNoteFreq(i+1)*(j))*65535)-((this->getFNoteFreq(i-1)*(128-j))*65535))/8192;
 	//wtjump_detune=(((((this->getFNoteFreq(i+12)*(j))*65535))/44100)-((((this->getFNoteFreq(i-12)*(128-j))*65535))/44100));
 	//wtjump_detune_1=(((this->getFNoteFreq(i+1)*65535)/44100)*16384)*j;
 	//wtjump_detune_2=(((this->getFNoteFreq(i-1)*65535)/44100)*16384)*(128-j);
 
-	wtjump_detune_1=(((this->getFNoteFreq(i+2)*((16384*65535)/44100))))*j;
-	wtjump_detune_2=(((this->getFNoteFreq(i-2)*((16384*65535)/44100))))*(128-j);
+	wtjump_detune_1=(((this->getFNoteFreq(i+1)*((WAVETABLE_SIZE*(wtshift))/44100))))*j;
+	wtjump_detune_2=(((this->getFNoteFreq(i-1)*((WAVETABLE_SIZE*(wtshift))/44100))))*(128-j);
 
 	//wtjump_detune=(wtjump_detune_1-wtjump_detune_2)*512;
-	wtjump_detune=(wtjump_detune_1-wtjump_detune_2)/512;
+	//wtjump_detune=(wtjump_detune_1-wtjump_detune_2)/512;
+	wtjump_detune=(wtjump_detune_1-wtjump_detune_2)/4096;
 	  
 
 	//-((((this->getFNoteFreq(i-12)*(128-j))*65535))/44100));
