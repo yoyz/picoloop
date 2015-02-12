8087bde7ca7d1a05e7c00771931eb4fbef5940c6
yoyz <johann.peyrard@bull.net>
2015-02-12 12:14:12 +0100

    cosmetic

--- cursynth/main.cpp
+++ cursynth/main.cpp
@@ -3,37 +3,37 @@
 #include <SDL/SDL.h>
 int main()
 {
-mopo::mopo_float 	* out_buffer_f;
-Sint16 			* out_buffer_i;
-
-int n_frames=256;
-int i;
-int j;
-int fd;
-out_buffer_f=(mopo::mopo_float*)malloc(sizeof(mopo::mopo_float)*8192);
-out_buffer_i=(Sint16*)malloc(sizeof(Sint16*)*8192);
-FILE     * FD;
-
-mopo::CursynthEngine CS;
-//CS.setBufferSize(8192);
-
-CS.setBufferSize(256);
-CS.setSampleRate(44100);
-CS.noteOn(30,1.0);
-//CS.processAudio((mopo::mopo_float*)out_buffer, n_frames);
-FD=fopen("audioout","w+");
-
-for (j=0;j<256;j++)
+  mopo::mopo_float 	* out_buffer_f;
+  Sint16 			* out_buffer_i;
+  
+  int n_frames=256;
+  int i;
+  int j;
+  int fd;
+  out_buffer_f=(mopo::mopo_float*)malloc(sizeof(mopo::mopo_float)*8192);
+  out_buffer_i=(Sint16*)malloc(sizeof(Sint16*)*8192);
+  FILE     * FD;
+  
+  mopo::CursynthEngine CS;
+  //CS.setBufferSize(8192);
+  
+  CS.setBufferSize(256);
+  CS.setSampleRate(44100);
+  CS.noteOn(40,1.0);
+  //CS.processAudio((mopo::mopo_float*)out_buffer, n_frames);
+  FD=fopen("audioout","w+");
+  
+  for (j=0;j<256;j++)
+    {
+      CS.process();
+      for (i=0;i<n_frames;i++)
 	{
-		CS.process();
-		for (i=0;i<n_frames;i++)
-		{
-			printf("%f\t",CS.output()->buffer[i]);
-			out_buffer_f[i]=CS.output()->buffer[i];
-			out_buffer_f[i]=out_buffer_f[i]*16384;
-			out_buffer_i[i]=out_buffer_f[i];
-		}
-		fwrite(out_buffer_i,sizeof(Sint16),256,FD);
+	  printf("%f\t",CS.output()->buffer[i]);
+	  out_buffer_f[i]=CS.output()->buffer[i];
+	  out_buffer_f[i]=out_buffer_f[i]*16384;
+	  out_buffer_i[i]=out_buffer_f[i];
 	}
-fclose(FD);
+      fwrite(out_buffer_i,sizeof(Sint16),256,FD);
+    }
+  fclose(FD);
 }
