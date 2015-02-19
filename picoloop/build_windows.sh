c31b934c2feb614174edaa843e1479c16062f830
yoyz <johann.peyrard@bull.net>
2015-02-19 14:28:38 +0100

    Validate the build on psp, debian, windows

--- picoloop/build_windows.sh
+++ picoloop/build_windows.sh
@@ -3,6 +3,6 @@ make       -f Makefile.RtAudio_mingw32                              clean
 make       -f Makefile.PatternPlayer_windows_mingw_RtAudio clean
 
 make       -f Makefile.RtAudio_mingw32                              
-make       -f Makefile.PatternPlayer_windows_mingw_RtAudio -j 64
+make       -f Makefile.PatternPlayer_windows_mingw_RtAudio -j 16
 
 
