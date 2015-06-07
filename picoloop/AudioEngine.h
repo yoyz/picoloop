b23eaf4b51d0d96a1656590b99c420ec7a620d79
yoyz <yoyz2k@yahoo.fr>
2015-06-07 17:41:37 +0200

    implement korg volca sync need to test it on multiple device with a good audio cable.

--- picoloop/AudioEngine.h
+++ picoloop/AudioEngine.h
@@ -44,12 +44,13 @@ public:
 
   int nb_tick_before_step_change;
   int nb_tick;
-  int nb_tick_before_step_change_div_six;
+  int nb_tick_before_step_change_real;
   int tick_length_high;
   int tick_length_low;
   int tick_height_high;
   int tick_height_low;
   int tick_height_std;
+  int step;
 
 };
 
