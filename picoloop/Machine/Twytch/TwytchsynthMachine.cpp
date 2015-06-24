19329ab9d3a2038d0282dcbfa2eb551a5d569dd0
yoyz <yoyz2k@yahoo.fr>
2015-06-25 01:43:12 +0200

    various modification to protect the twytch engine

--- picoloop/Machine/Twytch/TwytchsynthMachine.cpp
+++ picoloop/Machine/Twytch/TwytchsynthMachine.cpp
@@ -1,8 +1,9 @@
 #include "Master.h"
 #include "TwytchsynthMachine.h"
 
-//#define SAM 256
+
 #define SAM 64
+
 
 TwytchsynthMachine::TwytchsynthMachine()
 {
@@ -12,7 +13,51 @@ TwytchsynthMachine::TwytchsynthMachine()
   note_on=0;
   buffer_i=0;
   buffer_f=0;
+  pole=0;
+  need_note_on=0;
 
+  bpm=-1;
+  osc1_type=-1;
+  osc2_type=-1;
+  osc3_type=-1;
+  lfo1_waveform=-1;
+  lfo2_waveform=-1;
+  
+  adsr_env0_attack=-1;
+  adsr_env0_decay=-1;
+  adsr_env0_sustain=-1;
+  adsr_env0_release=-1;
+  
+  
+  adsr_env1_attack=-1;
+  adsr_env1_decay=-1;
+  adsr_env1_sustain=-1;
+  adsr_env1_release=-1;
+  
+  lfo1_env_amount=-1;
+  lfo2_env_amount=-1;
+  
+  osc12_mix=-1;
+  velocity=-1;
+  osc1_detune=-1;
+  osc1_mod=-1;
+  
+  osc1_amp=-1;
+  osc2_amp=-1;
+  osc3_amp=-1;
+  osc4_amp=-1;
+  
+  osc1_unison=-1;
+  osc2_unison=-1;
+  osc1_unisondt=-1;
+  osc2_unisondt=-1;
+  
+  filter1_type=-1;
+  filter1_saturation=-1;
+  filter1_feedback=-1;
+  filter1_cutoff=-1;
+  filter1_resonance=-1;
+  
 }
 
 
@@ -33,9 +78,8 @@ void TwytchsynthMachine::init()
       buffer_f==0)
     {
       TWE=new mopotwytchsynth::HelmEngine();
-      //CS->setBufferSize(SAM);     
-      buffer_i = (Sint16*)malloc(sizeof(Sint16)*SAM*8);
-      buffer_f = (mopotwytchsynth::mopo_float*)malloc(sizeof(mopotwytchsynth::mopo_float)*SAM*8);
+      buffer_i = (Sint16*)malloc(sizeof(Sint16)*SAM);
+      buffer_f = (mopotwytchsynth::mopo_float*)malloc(sizeof(mopotwytchsynth::mopo_float)*SAM);
 
       TWE->setBufferSize(SAM);
       TWE->setSampleRate(44100);
@@ -45,40 +89,27 @@ void TwytchsynthMachine::init()
     buffer_i[i]=0;
   for (i=0;i<SAM;i++)
     buffer_f[i]=0.0;
-
-  //TWE->getControls().at("delay time")->set(0.1);      
-  //TWE->getControls().at("delay dry/wet")->set(0.2);   
-  // TWE->getControls().at("delay time")->set(0.0);      
-  // TWE->getControls().at("delay dry/wet")->set(0.0);   
-
-  // TWE->getControls().at("delay feedback")->set(0.4);
-  // TWE->getControls().at("cross modulation")->set(0);  
-
-  // TWE->getControls().at("osc 2 transpose")->set(0); 
-  // TWE->getControls().at("osc 2 tune")->set(0); 
-  // TWE->getControls().at("volume")->set(0.1);
-
-  // TWE->getControls().at("lfo 1 waveform")->set(2); 
-  // TWE->getControls().at("lfo 2 waveform")->set(2); 
-
-
-  // TWE->getControls().at("mod source 1")->set(3);     
-  // TWE->getControls().at("mod source 2")->set(4);     
-  // TWE->getControls().at("mod destination 1")->set(4);     
-  // TWE->getControls().at("mod destination 2")->set(2);     
-  // TWE->getControls().at("mod scale 1")->set(0.9);     
-  // TWE->getControls().at("mod scale 2")->set(0.9);     
-  // TWE->getControls().at("lfo 1 frequency")->set(2.2);     
-  // TWE->getControls().at("lfo 1 frequency")->set(3.2);     
-
   TWE->getControls().at("filter_type")->set(0);
   TWE->getControls().at("beats_per_minute")->set(120);
-  //bq.reset();
+  TWE->getControls().at("legato")->set(0);
+  TWE->getControls().at("arp_on")->set(0);
+  TWE->getControls().at("arp_pattern")->set(0);
+  TWE->getControls().at("arp_sync")->set(1);
+  TWE->getControls().at("formant_on")->set(0);
+  TWE->getControls().at("osc_feedback_transpose")->set(-12);
+  TWE->getControls().at("osc_feedback_tune")->set(0);
+  TWE->getControls().at("step_frequency")->set(3);
+  TWE->getControls().at("stutter_on")->set(0);
+  TWE->getControls().at("volume")->set(0.6);
+  TWE->getControls().at("delay_on")->set(0);
+  TWE->getControls().at("delay_sync")->set(1);
+  TWE->getControls().at("filter_saturation")->set(0);
+  TWE->getControls().at("portamento_type")->set(0);
+  TWE->getControls().at("portamento")->set(-9);
+  TWE->getControls().at("reverb_on")->set(0);
+
   note_on=0;
-
   sample_num=0;
-
-
 
   note=0;
   detune=64;
@@ -104,8 +135,15 @@ int TwytchsynthMachine::checkI(int what,int val)
 
 
 
+    case ADSR_ENV0_ATTACK:
+      if (val<=3) return 3;
+      if (val>=127) return 127;
+      return val;
+      break;
+
+
     case TRIG_TIME_DURATION:
-      if (val<=0) return 1;
+      if (val<=2) return 2;
       if (val>=127) return 127;
       return val;
       break;
@@ -178,15 +216,8 @@ void TwytchsynthMachine::setF(int what,float val)
   float f_val=val;
   f_val=f_val/128;
 
-  //if (what==LFO1_FREQ)             TWE->getControls().at("lfo 1 frequency")->set(f_val*10); 
-  if (what==LFO1_FREQ)             TWE->getControls().at("mono_lfo_1_tempo"    )->set(f_val*11);
-  //if (what==LFO1_FREQ)             TWE->getControls().at("mono_lfo_1_frequency")->set(((f_val*2)-1)*6); 
-  //if (what==LFO1_FREQ)             TWE->getControls().at("mono_lfo_1_sync"    )->set(2);
-
-  
-  if (what==LFO2_FREQ)             TWE->getControls().at("mono_lfo_2_tempo"    )->set(f_val*11); 
-  //if (what==LFO2_FREQ)             TWE->getControls().at("mono_lfo_2_frequency")->set(((f_val*2)-1)*6); 
-  //if (what==LFO1_FREQ)             TWE->getControls().at("mono_lfo_2_sync"    )->set(2);
+  // if (what==LFO1_FREQ && lfo1_freq!=val)  { lfo1_freq=val; TWE->getControls().at("mono_lfo_1_tempo"    )->set(f_val*11); }
+  // if (what==LFO2_FREQ && lfo2_freq!=val)  { lfo2_freq=val; TWE->getControls().at("mono_lfo_2_tempo"    )->set(f_val*11); }
 }
 
 void TwytchsynthMachine::setI(int what,int val)
@@ -202,31 +233,25 @@ void TwytchsynthMachine::setI(int what,int val)
 
   if (what==NOTE_ON && val==1) 
     { 
-      NoteFreq & NF = NoteFreq::getInstance(); 
       note_on=1;
 
       if (old_note!=note)
 	{
-	  TWE->noteOn(note-1,velocity/32);
+	  TWE->noteOn(note-1);
 	  TWE->noteOff(old_note-1);
+	  need_note_on=1;
 	}
       else
 	{
 	  if (note_on==0)
-	    TWE->noteOn(note-1,velocity/32);
+	    {
+	      TWE->noteOn(note-1);
+	    }
 	  else
 	    {
-	      TWE->noteOff(old_note-1);	  
-	      TWE->noteOn(note-1,velocity/32);
+	      TWE->noteOn(note-1);
 	    }
 	}
-
-      //CS->noteOn(NF.getINoteFreq(note),1.0);
-      //TWE->noteOn(note+11,0.8);
-      //TWE->noteOn(note+11,velocity/32);
-      //TWE->getControls();
-      //f_val=trig_time_duration;
-      //TWE->getControls().at("velocity track")->set(trig_time_duration);
     }
   if (what==NOTE_ON && val==0) 
     { 
@@ -234,110 +259,93 @@ void TwytchsynthMachine::setI(int what,int val)
       TWE->noteOff(note-1);
     }
 
-  //if (what==OSC1_NOTE)           { if (note_on) { this->setI(NOTE_ON,0); note=val; } }
   if (what==OSC1_NOTE)              {  old_note=note; note=val; }
 
-  //if (what==ADSR_ENV0_ATTACK)    TWE->getControls().at("amp_attack")->set(val);
-  // //delay time"
-  // //if (what==ADSR_ENV0_ATTACK)    TWE->getControls().at("delay time")->set(f_val);
-  // //if (what==ADSR_ENV0_RELEASE)   TWE->getControls().at("delay dry/wet")->set(f_val);
-  // //if (what==ADSR_ENV0_DECAY)     TWE->getControls().at("delay feedback")->set(f_val);
+  if (what==BPM && bpm!=val)                              { bpm=val; TWE->getControls().at("beats_per_minute")->set(val);     }
 
-  if (what==BPM)                 { TWE->getControls().at("beats_per_minute")->set(val);     }
+  if (what==OSC1_TYPE && osc1_type!=val )                 { osc1_type=val; TWE->getControls().at("osc_1_waveform")->set(f_val*128); }
+  if (what==OSC2_TYPE && osc2_type!=val )                 { osc2_type=val; TWE->getControls().at("osc_2_waveform")->set(f_val*128); }
 
-  if (what==OSC1_TYPE)           { TWE->getControls().at("osc_1_waveform")->set(f_val*128); }
-  if (what==OSC2_TYPE)           { TWE->getControls().at("osc_2_waveform")->set(f_val*128); }
+  if (what==OSC3_TYPE && osc3_type!=val )                 { osc3_type=val; TWE->getControls().at("sub_waveform"  )->set(f_val*128); }
 
-  if (what==OSC3_TYPE)           { TWE->getControls().at("sub_waveform"  )->set(f_val*128); }
-
-  if (what==LFO1_WAVEFORM)       { TWE->getControls().at("mono_lfo_1_waveform")->set(f_val*128); }
-  if (what==LFO2_WAVEFORM)       { TWE->getControls().at("mono_lfo_2_waveform")->set(f_val*128); }
+  if (what==LFO1_WAVEFORM && lfo1_waveform!=val)          { lfo1_waveform=val; TWE->getControls().at("mono_lfo_1_waveform")->set(f_val*128); }
+  if (what==LFO2_WAVEFORM && lfo2_waveform!=val)          { lfo2_waveform=val; TWE->getControls().at("mono_lfo_2_waveform")->set(f_val*128); }
 
 
-   if (what==ADSR_ENV0_ATTACK)    TWE->getControls().at("amp_attack")->set(f_val*4);
-   if (what==ADSR_ENV0_DECAY)     TWE->getControls().at("amp_decay")->set(f_val*4);
-   if (what==ADSR_ENV0_SUSTAIN)   TWE->getControls().at("amp_sustain")->set(f_val);
-   if (what==ADSR_ENV0_RELEASE)   TWE->getControls().at("amp_release")->set(f_val*4);
+  if (what==ADSR_ENV0_ATTACK && adsr_env0_attack!=val)    { adsr_env0_attack=val;  TWE->getControls().at("amp_attack")->set(f_val*4);  }
+  if (what==ADSR_ENV0_DECAY  && adsr_env0_decay!=val)     { adsr_env0_decay=val;   TWE->getControls().at("amp_decay")->set(f_val*4);   }
+  if (what==ADSR_ENV0_SUSTAIN && adsr_env0_sustain!=val)  { adsr_env0_sustain=val; TWE->getControls().at("amp_sustain")->set(f_val);   }
+  if (what==ADSR_ENV0_RELEASE && adsr_env0_release!=val)  { adsr_env0_release=val; TWE->getControls().at("amp_release")->set(f_val*4); }
 
-   if (what==ADSR_ENV1_ATTACK)    TWE->getControls().at("fil_attack")->set(f_val*4);
-   if (what==ADSR_ENV1_DECAY)     TWE->getControls().at("fil_decay")->set(f_val*4);
-   if (what==ADSR_ENV1_SUSTAIN)   TWE->getControls().at("fil_sustain")->set(f_val);
-   if (what==ADSR_ENV1_RELEASE)   TWE->getControls().at("fil_release")->set(f_val*4);
+  if (what==ADSR_ENV1_ATTACK  && adsr_env1_attack!=val)   { adsr_env1_attack=val;  TWE->getControls().at("fil_attack")->set(f_val*4);  }
+  if (what==ADSR_ENV1_DECAY   && adsr_env1_decay!=val)    { adsr_env1_decay=val;   TWE->getControls().at("fil_decay")->set(f_val*4);   }
+  if (what==ADSR_ENV1_SUSTAIN && adsr_env1_sustain!=val)  { adsr_env1_sustain=val; TWE->getControls().at("fil_sustain")->set(f_val);   }
+  if (what==ADSR_ENV1_RELEASE && adsr_env1_release!=val)  { adsr_env1_release=val; TWE->getControls().at("fil_release")->set(f_val*4); }
 
 
-   if (what==LFO1_ENV_AMOUNT)     
+   if (what==LFO1_ENV_AMOUNT && lfo1_env_amount!=val)     
      { 
+       lfo1_env_amount=val;
        f_env1_amount=abs(abs(f_val-0.5)*2); 
        TWE->getControls().at("mono_lfo_1_amplitude")->set(((f_val*2)-1)*4);
      }
 
-   if (what==LFO2_ENV_AMOUNT)     
+   if (what==LFO2_ENV_AMOUNT && lfo2_env_amount!=val)     
      { 
+       lfo2_env_amount=val;
        f_env2_amount=abs(abs(f_val-0.5)*2); 
        TWE->getControls().at("mono_lfo_2_amplitude")->set(((f_val*2)-1)*4); 
-
-       TWE->clearModulations();
-       mopotwytchsynth::ModulationConnection * connection =
-	 new mopotwytchsynth::ModulationConnection("mono_lfo_1","keytrack");
-       connection->amount.set(f_env1_amount);
-       TWE->connectModulation(connection);
+        TWE->clearModulations();
+        mopotwytchsynth::ModulationConnection * connection =
+        	 new mopotwytchsynth::ModulationConnection("mono_lfo_1","keytrack");
+        connection->amount.set(f_env1_amount);
+        TWE->connectModulation(connection);
 
      }
 
 
 
-   if (what==OSC12_MIX)           TWE->getControls().at("osc_mix")->set(f_val);
+   if (what==OSC12_MIX && osc12_mix!=val)             { osc12_mix=val;  TWE->getControls().at("osc_mix")->set(f_val); }
 
-   //if (what==OSC1_AMP)            TWE->getControls().at("velocity_track")->set((f_val*2)-1);
-   //if (what==OSC1_AMP)            TWE->getControls().at("")->set(f_val);
-
-   if (what==VELOCITY)            TWE->getControls().at("velocity_track"     )->set(abs(f_val-1));
+   if (what==VELOCITY && velocity!=val)               { velocity=val;   TWE->getControls().at("velocity_track"     )->set(abs(f_val-1)); }
 
 
-   if (what==OSC1_DETUNE)         TWE->getControls().at("osc_2_tune"   )->set((f_val*2)-1);
-   if (what==OSC1_MOD)            TWE->getControls().at("cross_modulation")->set(f_val/2);
+   if (what==OSC1_DETUNE && osc1_detune!=val)         { detune=val;     TWE->getControls().at("osc_2_tune"   )->set((f_val*2)-1); }
+   if (what==OSC1_MOD    && osc1_mod!=val)            { osc1_mod=val;   TWE->getControls().at("cross_modulation")->set(f_val/2); }
 
-   //if (what==OSC1_AMP)            TWE->getControls().at("keytrack"     )->set(((f_val*2)-1)*128);
-   if (what==OSC1_AMP)            TWE->getControls().at("keytrack"     )->set(((f_val*2)-1));
-   if (what==OSC2_AMP)            TWE->getControls().at("fil_env_depth")->set(((f_val*2)-1)*128);
-
-
-   if (what==OSC3_AMP)            TWE->getControls().at("sub_volume"     )->set(f_val);
-   if (what==OSC4_AMP)            TWE->getControls().at("noise_volume"   )->set(f_val);
+   if (what==OSC1_AMP && osc1_amp!=val)               { osc1_amp=val;   TWE->getControls().at("keytrack"     )->set(((f_val*2)-1)); }
+   if (what==OSC2_AMP && osc2_amp!=val)               { osc2_amp=val;   TWE->getControls().at("fil_env_depth")->set(((f_val*2)-1)*128); }
 
 
-   if (what==OSC1_UNISON)         TWE->getControls().at("osc_1_unison_voices")->set(f_val*15);
-   if (what==OSC2_UNISON)         TWE->getControls().at("osc_2_unison_voices")->set(f_val*15);
+   if (what==OSC3_AMP && osc3_amp!=val)               { osc3_amp=val;   TWE->getControls().at("sub_volume"     )->set(f_val);}
+   if (what==OSC4_AMP && osc4_amp!=val)               { osc4_amp=val,   TWE->getControls().at("noise_volume"   )->set(f_val);}
 
 
-   if (what==OSC1_UNISONDT)       TWE->getControls().at("osc_1_unison_detune")->set(f_val*100);
-   if (what==OSC2_UNISONDT)       TWE->getControls().at("osc_2_unison_detune")->set(f_val*100);
+   if (what==OSC1_UNISON && osc1_unison!=val)         { osc1_unison=val; TWE->getControls().at("osc_1_unison_voices")->set(f_val*15); }
+   if (what==OSC2_UNISON && osc2_unison!=val)         { osc2_unison=val; TWE->getControls().at("osc_2_unison_voices")->set(f_val*15); }
 
-  // //if (what==OSC1_AMP)            TWE->getControls().at("velocity track")->set(f_val);
+
+   if (what==OSC1_UNISONDT && osc1_unisondt!=val)     { osc1_unisondt=val; TWE->getControls().at("osc_1_unison_detune")->set(f_val*100); }
+   if (what==OSC2_UNISONDT &&osc2_unisondt!=val)      { osc2_unisondt=val; TWE->getControls().at("osc_2_unison_detune")->set(f_val*100); }
+
   
-   if (what==FILTER1_TYPE)        TWE->getControls().at("filter_type")->set(f_val*128);
+   if (what==FILTER1_TYPE && filter1_type!=val)             { filter1_type=val; TWE->getControls().at("filter_type")->set(f_val*128); }
 
-   if (what==FILTER1_SATURATION)  TWE->getControls().at("filter_saturation")->set(((f_val*2)-1)*20);
-   if (what==FILTER1_FEEDBACK)    TWE->getControls().at("osc_feedback_amount")->set(((f_val*2)-1)*1);
-   //filter.setFilterType(val);
-  // //"filter type"
+   if (what==FILTER1_SATURATION && filter1_saturation!=val) { filter1_saturation=val; TWE->getControls().at("filter_saturation")->set(((f_val*2)-1)*20); }
+   if (what==FILTER1_FEEDBACK   && filter1_feedback!=val)   { filter1_feedback=val; TWE->getControls().at("osc_feedback_amount")->set(((f_val*2)-1)*1); }
 
 
-   if (what==FILTER1_CUTOFF)      
+   if (what==FILTER1_CUTOFF && filter1_cutoff!=val)      
      { 
-       //TWE->getControls().at("cutoff")->set(28+f_val*100);
+       filter1_cutoff=val;
        TWE->getControls().at("cutoff")->set(28+f_val*99);
      }
    
-   if (what==FILTER1_RESONANCE)         
+   if (what==FILTER1_RESONANCE && filter1_resonance!=val)         
      { 
-       //TWE->getControls().at("resonance")->set(0.5+(f_val*10));
+       filter1_resonance=val;
        TWE->getControls().at("resonance")->set(f_val);
-     }
-
-  // if (what==FX1_DEPTH)          TWE->getControls().at("delay dry/wet")->set(f_val*128);   
-  // if (what==FX1_SPEED)          TWE->getControls().at("delay time"   )->set(f_val*128);      
-  
+     }  
 
 }
 
@@ -360,7 +368,6 @@ const char * TwytchsynthMachine::getMachineParamCharStar(int machineParam,int pa
   static const char * str_ninepyramid   = "NPYR ";
   static const char * str_whitenoise    = "NOISE";
 
-  //static const char * str_wtbl          = "WTBL ";
 
   const        char * str_osc[PICO_TWYTCHSYNTH_SIZE];
 
@@ -380,7 +387,7 @@ const char * TwytchsynthMachine::getMachineParamCharStar(int machineParam,int pa
   static const char * str_fltr_type_bs   = "BS";
   static const char * str_fltr_type_ap   = "AP";
   static const char * str_fltr_type_nt   = "NT";
-  //static const char * str_fltr_type_hp   = "HP";
+
 
   const        char * str_fltr_type[TWYTCHSYNTH_FILTER_TYPE_SIZE];
 
@@ -408,11 +415,6 @@ const char * TwytchsynthMachine::getMachineParamCharStar(int machineParam,int pa
   str_osc[PICO_TWYTCHSYNTH_FIVEPYRAMID]      = str_fivepyramid;
   str_osc[PICO_TWYTCHSYNTH_NINEPYRAMID]      = str_ninepyramid;
 
-  //str_osc[PICO_TWYTCHSYNTH_WHITENOISE]       = str_whitenoise;
-
-  // str_fltr_algo[FILTER_ALGO_NOFILTER] = str_fltr_algo_nofilter;
-  // str_fltr_algo[FILTER_ALGO_BIQUAD]   = str_fltr_algo_biquad;
-  // str_fltr_algo[FILTER_ALGO_AMSYNTH]  = str_fltr_algo_amsynth;
 
   str_fltr_type[TWYTCHSYNTH_FILTER_TYPE_LP]       = str_fltr_type_lp;
   str_fltr_type[TWYTCHSYNTH_FILTER_TYPE_HP]       = str_fltr_type_hp;
@@ -422,7 +424,7 @@ const char * TwytchsynthMachine::getMachineParamCharStar(int machineParam,int pa
   str_fltr_type[TWYTCHSYNTH_FILTER_TYPE_BS]       = str_fltr_type_bs;
   str_fltr_type[TWYTCHSYNTH_FILTER_TYPE_AP]       = str_fltr_type_ap;
   str_fltr_type[TWYTCHSYNTH_FILTER_TYPE_NT]       = str_fltr_type_nt;
-  //str_fltr_type[FILTER_TYPE_HP]       = str_fltr_type_hp;
+
 
   str_lfo_type[LFO_TYPE_PITCHLFO]     = str_lfo_type_lfo;
   str_lfo_type[LFO_TYPE_PITCHBEND]    = str_lfo_type_pb;
@@ -437,8 +439,6 @@ const char * TwytchsynthMachine::getMachineParamCharStar(int machineParam,int pa
     case OSC3_TYPE:
       return str_osc[paramValue];
      
-    // case FILTER1_ALGO:
-    //   return str_fltr_algo[paramValue];
 
     case FILTER1_TYPE:
       return str_fltr_type[paramValue];
@@ -485,12 +485,14 @@ int TwytchsynthMachine::tick()
   Sint32 tmp1;
   Sint32 tmp2;
   Sint32 tmp3;
-  //Sint16 index;
 
   Sint32 cutoff_tmp;
   Sint32 resonance_tmp;
   int    num=8192;
   int    i;
+
+  float  buf_f;
+  Sint16 buf_i;
 
 
   if (index>=SAM | 
@@ -503,6 +505,12 @@ int TwytchsynthMachine::tick()
       index==0 )
     {
       TWE->process();
+      for (i=0;i<SAM;i++)
+	{
+	  buf_f=TWE->output(0)->buffer[i];
+	  buf_f=buf_f*8192;
+	  buffer_i[i]=buf_f;
+	}
     }
 
   if (trig_time_mode)
@@ -511,21 +519,13 @@ int TwytchsynthMachine::tick()
 	{
 	  this->setI(NOTE_ON,0);
 	  trig_time_mode=0;
-	  //printf("\t\t\t\t\t\tDONE\n");
 	}
 
     }
 
- 
-  buffer_f[index]=TWE->output()->buffer[index];
-  buffer_f[index]=buffer_f[index]*8192;
-  //buffer_f[index]=buffer_f[index]*14336;
-  //buffer_f[index]=buffer_f[index]*2048;
-  //buffer_f[index]=buffer_f[index]*1024;
-  buffer_i[index]=buffer_f[index];
-    
+  
   s_out=buffer_i[index];
-
+  
   index++;
   sample_num++;
   last_sample=s_out;
--- picoloop/Machine/Twytch/TwytchsynthMachine.h
+++ picoloop/Machine/Twytch/TwytchsynthMachine.h
@@ -48,13 +48,64 @@ class TwytchsynthMachine : public Machine
   int                   trig_time_duration;
   Sint32                trig_time_duration_sample;
 
-  int                   osc1_type;
-  int                   osc2_type;
+  //int                   osc1_type;
+  //int                   osc2_type;
 
-  int                   velocity;
+  //int                   velocity;
 
   float                 f_env1_amount;
   float                 f_env2_amount;
+
+  int                   pole;
+  int                   need_note_on;
+
+
+
+  int                   bpm;
+  int                   osc1_type;
+  int                   osc2_type;
+  int                   osc3_type;
+  int                   lfo1_waveform;
+  int                   lfo2_waveform;
+
+  int                   adsr_env0_attack;
+  int                   adsr_env0_decay;
+  int                   adsr_env0_sustain;
+  int                   adsr_env0_release;
+
+
+  int                   adsr_env1_attack;
+  int                   adsr_env1_decay;
+  int                   adsr_env1_sustain;
+  int                   adsr_env1_release;
+
+  int                   lfo1_env_amount;
+  int                   lfo2_env_amount;
+  
+  int                   osc12_mix;
+  int                   velocity;
+  int                   osc1_detune;
+  int                   osc1_mod;
+
+  int                   osc1_amp;
+  int                   osc2_amp;
+  int                   osc3_amp;
+  int                   osc4_amp;
+  
+  int                   osc1_unison;
+  int                   osc2_unison;
+  int                   osc1_unisondt;
+  int                   osc2_unisondt;
+
+  int                   filter1_type;
+  int                   filter1_saturation;
+  int                   filter1_feedback;
+  int                   filter1_cutoff;
+  int                   filter1_resonance;
+
+  float                 lfo1_freq;
+  float                 lfo2_freq;
+
 
 };
 
