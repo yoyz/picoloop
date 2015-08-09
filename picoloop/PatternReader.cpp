0bfc0d7371b45955354fbc5f4cfcd23d1121335f
yoyz <yoyz2k@yahoo.fr>
2015-08-09 17:35:19 +0200

    fix the song between bank

--- picoloop/PatternReader.cpp
+++ picoloop/PatternReader.cpp
@@ -85,13 +85,18 @@ int PatternReader::saveSong(SongSequencer & SS)
 int PatternReader::loadSong(SongSequencer & SS)
 {
   char  filename[1024];
-  unsigned char line[MAX_PATTERN_BY_PROJECT];
+  unsigned char line[MAX_PATTERN_BY_PROJECT]={0};
   int i;
   int j;
   sprintf(filename,"bank/bank%d/song.pic",bank);
   fd=fopen(filename,"r");
   if (fd==0)
     {
+      for (j=0;j<TRACK_MAX;j++)
+	for (i=0;i<MAX_PATTERN_BY_PROJECT;i++)
+	  {
+	    SS.songVector[i][j]=line[i];
+	  }
       return 0;
     }
   for (j=0;j<TRACK_MAX;j++)
