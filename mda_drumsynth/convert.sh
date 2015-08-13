cb2798bbb01f36c70c309fd476c8eecb5eb27cee
yoyz <yoyz2k@yahoo.fr>
2015-08-13 19:11:03 +0200

    quick and bad way to convert a lot of file

--- mda_drumsynth/convert.sh
+++ mda_drumsynth/convert.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-for i in $(find bin/|grep ds$) ; do name=$( echo $i| sed -e 's@/@_@g' -e 's@.ds@.wav@g' ) ; ./a.out $i wav/$name ; done
+#set -x 
+mkdir tmp
+cd tmp
+unzip ../drumsynth_2_0_0.zip
+mv bin/ ..
+cd ..
+cd bin
+unzip library.zip
+cd ..
+mkdir wav
+for i in $(find bin/|grep ds$) ; do name=$( echo $i| sed -e 's@ @_@g' -e 's@/@_@g' -e 's@.ds@.wav@g' ) ; ./a.out $i wav/$name ; done
