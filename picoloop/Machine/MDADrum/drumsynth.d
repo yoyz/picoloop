463f0d9b06d36052a154561f1708812cf796abf8
yoyz <yoyz2k@yahoo.fr>
2015-08-15 19:32:43 +0200

    this file should not be on the git

---
+++ picoloop/Machine/MDADrum/drumsynth.d
@@ -0,0 +1,2 @@
+drumsynth.o: Machine/MDADrum/drumsynth.cpp Machine/MDADrum/drumsynth.h \
+ Machine/MDADrum/profport.h
---
+++ picoloop/Machine/MDADrum/profport.d
@@ -0,0 +1,1 @@
+profport.o: Machine/MDADrum/profport.cpp Machine/MDADrum/profport.h
