686534e07deee51c3c455661d16e1008cf9d9e20
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2016-01-22 03:34:24 -0500

    remove noise file

--- picoloop/Machine/MDADrum/MDADrumMachine.d
+++
@@ -1,4 +0,0 @@
-MDADrumMachine.o: Machine/MDADrum/MDADrumMachine.cpp \
- Machine/MDADrum/MDADrumMachine.h Machine.h Master.h Filter.h Biquad.h \
- Oscillator.h WaveTableManager.h WaveTable.h NoteFreq.h \
- AMSynthLowPassFilter.h NoteFreq.h Machine/MDADrum/drumsynth.h
