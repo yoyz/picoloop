20b5ac8c370aa30b1552b82ffbe34813e87a9d17
yoyz <yoyz2k@yahoo.fr>
2016-07-26 23:19:52 +0200

    generate only the buffer in mda drum synth WIP

--- mda_drumsynth/build.sh
+++ mda_drumsynth/build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-g++ -fpermissive main.cpp profport.cpp  drumsynth.c
+# g++ -fpermissive main.cpp     profport.cpp  drumsynth.c      -o drumsynth
+# g++ -fpermissive main_oop.c   profport.cpp  drumsynth_oop.c  -o drumsynth_oop
+# g++ -fpermissive main_oop2.c   profport.cpp  drumsynth_oop2.c  -o drumsynth_oop2
+#g++ -fpermissive main_oop3.c   profport.cpp  drumsynth_oop3.c  -o drumsynth_oop3
+#g++ -ggdb -fpermissive main_oop4.c   profport.cpp  drumsynth_oop4.c  -o drumsynth_oop4
+g++ -ggdb -fpermissive main_oop5.c   profport.cpp  drumsynth_oop5.c  -o drumsynth_oop5
