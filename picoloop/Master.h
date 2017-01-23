bfc14d15bb5ed28fe7ac009ce19b75274adc2acc
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2017-01-23 17:51:15 +0100

    change version from 0.76a to 0.76b

--- picoloop/Master.h
+++ picoloop/Master.h
@@ -13,7 +13,7 @@
 #endif
 
 
-#define PICOLOOP_VERSION "0.76a"
+#define PICOLOOP_VERSION "0.76b"
 //#define INTERNAL_BUFFER_SIZE 128*1024   
 #define INTERNAL_BUFFER_SIZE 16*1024   
 //#define DEFAULTFREQ      44100
