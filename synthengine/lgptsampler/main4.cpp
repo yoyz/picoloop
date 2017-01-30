e49d94a9cf80c797a0bb229750fbc50fee8c071f
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2017-01-30 22:39:31 +0100

    able to change the filter and reso

--- synthengine/lgptsampler/main4.cpp
+++ synthengine/lgptsampler/main4.cpp
@@ -45,20 +45,37 @@ int main(int argc, char **argv)
       printf("%s\n",tab_str[i]);
     }
   SI.Init();
-  SI.AssignSample(0);
+  SI.AssignSample(1);
   
   printf("<<<<SP->GetSource(0)=%d>>>>>\n",SP->GetSource(0));
   printf("<<<<SP->GetSource(1)=%d>>>>>\n",SP->GetSource(1));
   printf("<<<<SI.GetSampleIndex()=%d>>>>>\n",SI.GetSampleIndex());
 
   FD=fopen("audioout","w+");
+      SI.ProcessCommand(0,MAKE_FOURCC('T','R','S','P'),j);
+
+      SI.ProcessCommand(0,MAKE_FOURCC('L','S','T','A'),0x10); 
+      SI.ProcessCommand(0,MAKE_FOURCC('L','L','E','N'),0x11);
+
+      SI.ProcessCommand(0,MAKE_FOURCC('L','M','O','D'),0x2);
+      SI.ProcessCommand(0,MAKE_FOURCC('S','T','R','T'),0x1);
+      SI.ProcessCommand(0,MAKE_FOURCC('E','N','D','_'),0x2);
     
   SI.Start(0,48,true);
   //SI.Render(0,buffer,1024,true);
-  for (j=0;j<128;j++)
+  for (j=0;j<1024;j++)
     {
       if (j==32)
 	SI.Start(0,72,true);
+      //SI.ProcessCommand(0,MAKE_FOURCC('F','C','U','T'),0x7F);
+      SI.ProcessCommand(0,MAKE_FOURCC('F','C','U','T'),0xFF);
+      SI.ProcessCommand(0,MAKE_FOURCC('F','R','E','S'),0);
+
+
+      SI.ProcessCommand(0,MAKE_FOURCC('D','S','P','L'),0);
+
+      SI.ProcessCommand(0,MAKE_FOURCC('V','O','L','M'),0x7F);
+      SI.ProcessCommand(0,MAKE_FOURCC('C','R','S','H'),0); 
 
       SI.Render(0,buffer,512,true);
       
@@ -69,12 +86,12 @@ int main(int argc, char **argv)
 	buffer16[i]=buffer[i];
 
       fwrite(buffer16,sizeof(int16_t),1024,FD);
-
+      /*
       for(i=0;i<1024;i++)
 	{
-      printf("0x%08.8X\t",buffer16[i]);
-    }
-
+	  printf("0x%08.8X\t",buffer16[i]);
+	}
+      */
 
     }
     free(buffer);
