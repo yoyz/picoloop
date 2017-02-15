ce37d6766154f17ca9a588513ca91150d2b6b47d
johann peyrard <yoyz2k@yahoo.fr>
2017-02-15 23:52:49 +0100

    an update for windows

--- picoloop/SYSTEMWINDOWS.cpp
+++ picoloop/SYSTEMWINDOWS.cpp
@@ -6,10 +6,27 @@
 #include <sys/stat.h>
 #include <stdio.h>
 #include <unistd.h>
+#define MAXHOMEPATH 256
 
 int MKDIR(const char *pathname)
 {
   mkdir(pathname);
 }
+char * GETPICOLOOPUSERSTORAGE()
+{
+  static char picoloophome[MAXHOMEPATH];
+  //snprintf(picoloophome,"%s/.picoloop/",GETHOME());
+  sprintf(picoloophome,"./");
+  return picoloophome;
+}
+
+char * GETHOME()
+{
+  static char homedir[MAXHOMEPATH];
+  //sprintf(homedir, MAXHOMEPATH, "%s", getenv("HOME"));
+  sprintf(homedir, MAXHOMEPATH, "./");
+  return homedir;
+}
+
 
 #endif
