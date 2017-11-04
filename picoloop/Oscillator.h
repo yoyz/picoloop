e453a23d90b5a47ab7f502bace902b2a6fc18c85
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2017-11-04 04:45:20 +0100

    remove unused variable from oscillator

--- picoloop/Oscillator.h
+++ picoloop/Oscillator.h
@@ -4,6 +4,7 @@ using namespace std;
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <assert.h>
 //#include <SDL/SDL.h>
 
 #ifdef   __SDL12__               
@@ -26,6 +27,7 @@ class Oscillator
  public:
   Oscillator();
   ~Oscillator();
+  void   init();
   void   setAmplitude(int amp);
   void   reset();
   void   setPhase(int phase);
@@ -42,14 +44,9 @@ class Oscillator
 
 
 
-  Sint16 last_tick;
-  //int    length;
   int    amplitude;
   int    frequency;
-  int    bit;
   int    sample_num;
-  int    sample_num_index;
-  int    table_fill;
   int    table_size;
   int    freq;
   int    note;
@@ -59,9 +56,6 @@ class Oscillator
   Sint32 offset_next_index; // index=index+(this->getFreq()*table_size)/44100;
                             // index=index+(offset_next)
                             // offset_next=(this->getFreq()*table_size)/44100;
-  Uint16 u;
-  Uint16 last_u;
-  Sint16 s;
   Sint16 ph;
 
   Sint32 index;
