e54ded28becdc507dff747d008c4d22c2e47fa42
johann peyrard <yoyz2k@yahoo.fr>
2017-12-03 20:42:23 +0100

    remove unused extern global variable

--- picoloop/MidiInSystem.h
+++ picoloop/MidiInSystem.h
@@ -48,7 +48,6 @@ extern int counter_recv_midi_clock;     // send n clock and decrement the counte
 extern int counter_recv_midi_clock_six; // send n clock and decrement the counter each tim
 extern int mmc_stop;                    // reset the step sequencer to step 0
 extern int mmc_start;                   // 
-extern int sequencer_play;              //
 
 class MidiInSystem
 {
